964e892448299ff62fed7b8d0fa58b2f29e51c10
Mark Hellewell <mch@tropism.io>
2020-05-09 23:33:14 +1000

    remap vol on plaid

--- keyboards/plaid/keymaps/markhellewell/keymap.c
+++ keyboards/plaid/keymaps/markhellewell/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 							      [1] = LAYOUT_plaid_mit(
 										     KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL,
 										     RESET, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_BSLS,
-										     KC_LSFT, KC_MUTE, KC_APP, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_MPLY, KC_VOLU, KC_VOLD, KC_PSCR, KC_SFTENT,
+										     KC_LSFT, KC_MUTE, KC_APP, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_MPLY, KC_PSCR, KC_VOLD, KC_VOLU, KC_SFTENT,
 										     KC_LCTL, KC_LGUI, KC_LALT, KC_NO, KC_NO, KC_SPC, KC_INS, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 										     ),
 							      [2] = LAYOUT_plaid_mit(
