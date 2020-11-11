6103a0c637ae2b4a4e52aade829ab67d42a285df
Mark Hellewell <mch@tropism.io>
2020-11-11 18:27:50 +1100

    tweaks to keymaps

--- keyboards/yd60mq/keymaps/markhellewell/keymap.c
+++ keyboards/yd60mq/keymaps/markhellewell/keymap.c
@@ -1,10 +1,5 @@
 #include QMK_KEYBOARD_H
 
-enum layer_names {
-    _BL,
-    _FL,
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     //,: Base Layer
@@ -19,8 +14,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //,: Function Layer
     [1] = LAYOUT_60_iso(
         RESET,  KC_F1,   KC_F2,    KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        XXXXXXX, KC_BTN1, KC_MS_U,  KC_BTN2,  KC_WH_U, XXXXXXX, XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, KC_PGUP, KC_HOME, KC_PSCR,
-        KC_TRNS, KC_MS_L, KC_MS_D,  KC_MS_R,  KC_WH_D, RGB_HUI, RGB_SAI, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_END,  XXXXXXX, KC_MNXT,
+        XXXXXXX, KC_BTN1, KC_UP,  KC_BTN2,  KC_WH_U, XXXXXXX, XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, KC_PGUP, KC_HOME, KC_PSCR,
+        KC_TRNS, KC_LEFT, KC_DOWN,  KC_RIGHT,  KC_WH_D, RGB_HUI, RGB_SAI, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_END,  XXXXXXX, KC_MNXT,
         KC_TRNS, RGB_MOD, RGB_RMOD, RGB_VAI,  RGB_VAD, RGB_HUD, RGB_SAD, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY,          KC_MPRV,
         KC_TRNS, KC_TRNS, KC_TRNS,                              RGB_TOG,                            RGB_SPI, RGB_SPD, XXXXXXX, KC_TRNS
 	)
