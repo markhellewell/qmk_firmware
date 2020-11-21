95e88ca965fede39daedecae518cdf6df57b5668
Mark Hellewell <mch@tropism.io>
2020-11-21 15:57:46 +1100

    improvements to the prototyper keymap

--- keyboards/prototyper/keymaps/default/keymap.c
+++ keyboards/prototyper/keymaps/default/keymap.c
@@ -18,7 +18,8 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
-    _FN
+    _LOWER,
+    _RAISE
 };
 
 // Defines the keycodes used by our macros in process_record_user
@@ -33,14 +34,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                            KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
                                                                            KC_GESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
                                                                            KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_SFTENT,
-                                                                           KC_LCTL, KC_LCMD, KC_LALT, KC_RALT, MO(1), KC_SPC, MO(2), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+                                                                           KC_LCTL, KC_LCMD, KC_LALT, KC_RALT, MO(_LOWER), KC_SPC, MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
                                                                            ),
                                                               [1] = LAYOUT(
-                                                                           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_EQL,
                                                                            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                                                                           KC_NO, KC_NO, KC_NO, KC_NO, DYN_REC_START1, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
                                                                            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                                                                           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                                                                           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, BL_STEP, BL_BRTG, KC_NO, KC_NO, KC_NO, KC_NO
+                                                                           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, DYN_REC_STOP, KC_NO, DYN_MACRO_PLAY1,
+                                                                           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
                                                                            ),
                                                               [2] = LAYOUT(
                                                                            KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_EQL,
